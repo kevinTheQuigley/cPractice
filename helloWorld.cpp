50ea4fb62b373425e7043d998ec4109dad2d0b88
Kevin Quigley <kevinquigley@Kevins-MacBook-Pro.local>
2024-11-13 11:59:28 +1100

    Saving settings files and comments

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,6 +1,12 @@
+// To build on mac use "option+ctrl+b", to run use "option+ctrl+r"
+// To build on win use "alt   +ctrl+b", to run use "alt   +ctrl+r"
 #include <iostream>
 
-int main() {
-    std::cout << "Hello World!" << std:: endl;
+int main()
+{
+    int favouriteNumber;
+    std:: cout << "Enter Your fav number:";
+    std:: cin >> favouriteNumber;
+    std:: cout << "Amazing! That's my favourite number to." << std::endl;
     return 0;
 }
